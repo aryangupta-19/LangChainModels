fa5718f90afd46eba334c7195794f376f69e6fc0
Aryan gupta <aryangupta4632@gmail.com>
2026-08-18 23:32:48 +0530

    Syntax changes

--- ChatModels/chatModel_hf_api.py
+++ ChatModels/chatModel_hf_api.py
@@ -1,25 +1,13 @@
-import os
-from pathlib import Path
-
-from dotenv import load_dotenv
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
 
-
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(env_path)
-
-print(
-    "Token loaded:",
-    os.getenv("HUGGINGFACEHUB_API_TOKEN") is not None
-)
-
+load_dotenv()
 
 llm = HuggingFaceEndpoint(
     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
-    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    # max_new_tokens=1280,
 )
-
 
 model = ChatHuggingFace(llm=llm)
 
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 import langchain
 print(langchain.__version__)
+
+# python ChatModels/chatModel_hf_api.py
